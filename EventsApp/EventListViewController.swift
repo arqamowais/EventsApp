98de1cecf5afd9787cf8ba733b278af044d111e3
Muhammad Arqam Owais <muhammadarqam912@gmail.com>
2020-09-29 01:02:00 +0500

    added Event List title and add button

--- EventsApp/EventListViewController.swift
+++ EventsApp/EventListViewController.swift
@@ -12,13 +12,31 @@ final class EventListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.setupViews()
     }
     
     static func instantiate() -> EventListViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: .main)
         let controller = storyboard.instantiateViewController(identifier: "EventListViewController") as! EventListViewController
         return controller
+    }
+    
+    // MARK: - Private Functions
+    
+    private func setupViews() {
+        let plusImage = UIImage(systemName: "plus.circle.fill")
+        let barButtonItem = UIBarButtonItem(image: plusImage, style: .plain, target: self, action: #selector(tappedRightBarButton))
+        barButtonItem.tintColor = UIColor.primary
+        
+        navigationItem.rightBarButtonItem = barButtonItem
+        navigationItem.title = "Events"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+        
+    @objc
+    private func tappedRightBarButton() {
+        
     }
 
 }
---
+++ EventsApp/UIColor+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Extension.swift
+//  EventsApp
+//
+//  Created by Arqam Owais on 29/09/2020.
+//  Copyright © 2020 Arqam Owais. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primary = UIColor(red: 0/255, green: 39/255, blue: 255/255, alpha: 0.6)
+}
